f81ed9a2c9539f8faf858360cf286488ec83cff1
angelovangel <aangeloo@gmail.com>
2024-01-29 07:27:59 +0100

    testing

--- app.R
+++ app.R
@@ -16,7 +16,8 @@ sidebar <- sidebar(
     "model", "Select dorado model",
     choices = c('fast', 'hac', 'sup')
   ),
-  numericInput("seconds", "Sleep secs", 3)
+  numericInput("seconds", "Sleep secs", 3),
+  actionButton('start', 'Start')
 )
 
 ui <- page_navbar(
@@ -29,11 +30,40 @@ ui <- page_navbar(
   card(max_height = '250px',
   reactableOutput('tmux_sessions')
   ),
+  card(max_height = '400px',
   verbatimTextOutput('stdout')
+  )
   )
 )
 
 server <- function(input, output, session) {
+  # reactives
+  
+  # track tmux sessions
+  tmux_sessions <- reactive({
+    invalidateLater(2000, session)
+    name <- system("tmux ls | cut -f1 -d:")
+    time <- system("tmux ls | cut -f6,7,8 -d' '")
+    
+    
+  })
+  
+  # observers
+  observeEvent(input$start, {
+    withCallingHandlers({
+      p <- processx::run(
+        'cowsay', args = c(as.character(input$seconds)),
+        stdout_line_callback = function(line, proc) { message(line) }, 
+        #stdout_callback = cb_count,
+        stderr_to_stdout = TRUE, 
+        error_on_status = FALSE
+      )
+    },
+    message = function(m) {
+      shinyjs::html(id = "stdout", html = m$message, add = TRUE);
+      runjs("document.getElementById('stdout').scrollTo(0,1e9);") # scroll the page to bottom with each message, 1e9 is just a big number
+    })
+  })
   # outputs
   output$tmux_sessions <- renderReactable({
     reactable(mtcars, pagination = FALSE, highlight = TRUE, height = 200, compact = F, fullWidth = FALSE,)
